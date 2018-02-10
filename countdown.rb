af80e43a3f25988fe2d221184a45ab47957e5bd3
Eva Smidt <shevangeline@gmail.com>
2018-02-10 02:42:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,10 +7,18 @@ puts "#{seconds_to_midnight} SECOND(S)!"
   seconds_to_midnight -= 1
 end
   #sleep(5.seconds)
-puts "HAPPY NEW YEAR!"
+ "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+   seconds_to_midnight -= 1
+ end 
+ "HAPPY NEW YEAR!"
+ end
+ 
 #Write a method that takes in an integer argument and uses a while loop
 #to countdown from that integer to 0, outputting "#{number} SECOND(S)!"
 #in each iteration of the loop. The method should return "HAPPY NEW YEAR!"
