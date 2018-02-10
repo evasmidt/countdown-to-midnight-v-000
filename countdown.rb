4a09fe90e0a16586830450953eb87d2988f6e0db
Eva Smidt <shevangeline@gmail.com>
2018-02-10 02:34:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,6 +6,7 @@ puts "#{number} SECOND(S)!"
   x += 1
 puts "HAPPY NEW YEAR!"
 end
+end
 #Write a method that takes in an integer argument and uses a while loop
 #to countdown from that integer to 0, outputting "#{number} SECOND(S)!"
 #in each iteration of the loop. The method should return "HAPPY NEW YEAR!"
